0841ee10ca86c126dfd539286882c2f77c63fbe5
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-05-25 20:50:09 +0200

    Add file getting package contents from the Web

---
+++ NoInstall.m
@@ -0,0 +1,4 @@
+(* ::Package:: *)
+
+Get["https://raw.githubusercontent.com/jkuczm/MathematicaTeXUtilities/master/TeXUtilities/TeXUtilities.m"]
+Get["https://raw.githubusercontent.com/jkuczm/MathematicaTeXUtilities/master/TeXUtilities/FormatTeXFormPatch.m"]
