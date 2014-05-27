dc94d3f2bebd92d2c6cc221c8c94a640b8b1bd54
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-05-27 15:28:34 +0200

    Use guide as documentation main page

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,16 +1,18 @@
 (* Paclet Info File *)
 
-(* created 2014/05/18*)
+(* created 2014/05/27*)
 
 Paclet[
     Name -> "TeXUtilities",
     Version -> "1.0.0",
-    MathematicaVersion -> "7+",
+    MathematicaVersion -> "6+",
     Description -> "Application providing tools useful for customizing TeX output of Mathematica.\n\nContains also patch fixing behavior of TeXForm for expressions with custom formatting defined using Format.",
     Creator -> "Jakub Kuczmarski",
     Extensions -> 
         {
-            {"Documentation", Language -> "English", MainPage -> "Tutorials/TeXUtilitiesOverview"}
+            {"Documentation", Resources -> 
+                {"Guides/TeXUtilities"}
+            , Language -> "English"}
         }
 ]
 
