e8af3488c3de6b9895295033e4b5b192b2263de9
jkuczm <Jakub.Kuczmarski@gmail.com>
2014-06-29 23:54:38 +0200

    Use BootstrapInstaller package

--- BootstrapInstall.m
+++ BootstrapInstall.m
@@ -1,42 +1,18 @@
 (* ::Package:: *)
 
-Module[
-	{$PIImportResult}
-	,
-	
-	Quiet[
-		$PIImportResult = Needs["ProjectInstaller`"],
-		{Get::noopen, Needs::nocont}
-	];
-	
-	If[$PIImportResult === $Failed,
-		Print["ProjectInstaller not found, installing it:"];
-		Print @ Import[
-			"https://raw.github.com/lshifr/ProjectInstaller/master/BootstrapInstall.m"
-		];
-		$PIImportResult = Needs["ProjectInstaller`"];
-	];
+Get["https://raw.githubusercontent.com/jkuczm/MathematicaBootstrapInstaller/v0.1.0/BootstrapInstaller.m"]
 
-	If[$PIImportResult === $Failed,
-		Print[
-			"Unable to load ProjectInstaller.\n",
-			"Please ",
+
+BootstrapInstall[
+	"TeXUtilities",
+	"https://github.com/jkuczm/MathematicaTeXUtilities/releases/download/v1.0.0/TeXUtilities.zip",
+	"AdditionalFailureMessage" -> 
+		Sequence[
+			"You can ", 
 			Hyperlink[
-				"install TeXUtilities package manually",
+				"install TeXUtilities package manually", 
 				"https://github.com/jkuczm/MathematicaTeXUtilities#manual-installation"
 			],
-			".\n",
-			"We would be grateful for ",
-			Hyperlink[
-				"reporting this issue",
-				"https://github.com/jkuczm/MathematicaTeXUtilities/issues"
-			],
 			"."
-		];
-	(* else *),
-		Print["Installing TeXUtilities:"];
-		Print @ ProjectInstaller`ProjectInstall @ URL[
-			"https://github.com/jkuczm/MathematicaTeXUtilities/releases/download/v1.0.0/TeXUtilities.zip"
-		];
-	];
+		]
 ]
